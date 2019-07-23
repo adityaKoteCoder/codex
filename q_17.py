151af7df43e52221b01c37f8bac954d5e4392199
dsouzajoy <akstr17official@gmail.com>
2019-07-23 13:06:40 +0530

    yes

---
+++ q_17.py
@@ -0,0 +1,9 @@
+data = "Rajesh , Krish, manoj , suresh, jayesh, mahesh, charan"
+names = data.upper().split(",")
+print(names)
+lst=[]
+for name in names:
+    if name.startswith("A") or name.endswith("H"):
+        lst.append(name)
+lst.sort()
+print(lst)
